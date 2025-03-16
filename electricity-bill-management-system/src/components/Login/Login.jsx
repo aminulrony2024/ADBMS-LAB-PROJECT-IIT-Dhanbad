71a09c1593442da411cf2e4be9eefc8cb2536a35
aminulrony2024 <aminulrony2024@gmail.com>
2025-03-16 20:36:50 +0530

    login page added

---
+++ electricity-bill-management-system/src/components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { app } from '../../firebase/firebase.config';
+import useAuth from "../../Hooks/useAuth";
+const auth = getAuth(app);
+const Login = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Login;
