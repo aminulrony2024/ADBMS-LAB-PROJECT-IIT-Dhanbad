f4a3683581ad49209018472cf33c96308ab71822
aminulrony2024 <aminulrony2024@gmail.com>
2025-03-16 20:19:35 +0530

    google authprovider added

---
+++ electricity-bill-management-system/src/providers/AuthProvider.jsx
@@ -0,0 +1,57 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+import { createContext, useEffect, useState } from "react";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
+const auth = getAuth(app);
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        const userInfo = { email: currentUser.email };
+      } else {
+        // Remove token (if token stored in the client side from local storage if the user is logged out)
+        //   localStorage.removeItem('access-token');
+        setLoading(false);
+      }
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, [axiosPublic]);
+  const newUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () => {
+    return signOut(auth);
+  };
+  const authInfo = {
+    user,
+    setUser,
+    setLoading,
+    loading,
+    newUser,
+    signIn,
+    logOut,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
