483d87f683f955567591eadc8d155de56c6d571d
aminulrony2024 <aminulrony2024@gmail.com>
2025-03-16 19:20:07 +0530

    tailwind css installed

---
+++ electricity-bill-management-system/src/Layout/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-green-500 font-bold underline'>ADBMS Lab Project IIT ISM Dhanbad</h1>
+        </div>
+    );
+};
+
+export default Home;
--- electricity-bill-management-system/src/Routes/Routes.jsx
+++ electricity-bill-management-system/src/Routes/Routes.jsx
@@ -2,11 +2,11 @@ import {
     createBrowserRouter,
     RouterProvider,
   } from "react-router-dom";
-import Main from "../components/Main";
+import Home from "../Layout/Home/Home";
   export const router = createBrowserRouter([
     {
       path: "/",
-      element: <Main></Main>
+      element: <Home></Home>
     },
   ]);
   
